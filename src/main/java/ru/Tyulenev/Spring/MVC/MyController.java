c80d947277c87734039f69581a5aa2b637fd297d
Alexey Tyulenev <alexey.tyulenev@q-systems.ru>
2022-05-11 11:11:04 +0300

    Added MyController.java

---
+++ src/main/java/ru/Tyulenev/Spring/MVC/MyController.java
@@ -0,0 +1,13 @@
+package ru.Tyulenev.Spring.MVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView() {
+        return "first-view";
+    }
+}
